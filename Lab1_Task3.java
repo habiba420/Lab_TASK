43e0e34c9887237bb8d123ae8d81aa004aa1b662
habiba420 <124070827+habiba420@users.noreply.github.com>
2023-02-06 22:54:12 +0600

    Add files via upload

---
+++ Lab1_Task3.java
@@ -0,0 +1,15 @@
+
+package java_basic;
+
+
+public class Lab1_Task3 {
+    public static void main(String[] args) {
+        String Name="Mst. Habiba Sultana";
+         
+        
+        System.out.println(Name);
+        
+        
+    }
+    
+}
